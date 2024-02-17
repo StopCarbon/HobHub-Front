ccc50b3360d057cdae2cb3e080cf04386f7e7d71
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2024-02-17 14:57:22 +0900

    ✨ : 로그인 페이지 퍼블리싱

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,11 +1,53 @@
 import styled from 'styled-components';
+import loginButton from '../assets/loginpage/kakao_login_button_sm.svg';
+import Logo from 'components/_common/Logo';
+import shadow from '../assets/_common/shadow.svg';
 
 const LoginPage = () => {
-  return (
-    <div>
-      <h1>create</h1>
-    </div>
-  );
+    return (
+        <Wrapper>
+            <Logo type="main"></Logo>
+            <Shadow src={shadow}></Shadow>
+            <LoginButton src={loginButton}></LoginButton>
+        </Wrapper>
+    );
 };
 
 export default LoginPage;
+
+const Wrapper = styled.div`
+    width: 100%;
+    height: 100vh;
+    background-color: var(--pink);
+    background-image: linear-gradient(
+        to top,
+        var(--blue2) 0%,
+        transparent 100%
+    );
+    background-size: cover;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const Shadow = styled.img`
+    width: calc(100% - 140px);
+    opacity: 0.5;
+
+    @media (min-width: 1024px) {
+        width: 330px;
+    }
+`;
+
+const LoginButton = styled.img`
+    width: calc(100% - 80px);
+    margin-top: 250px;
+    cursor: pointer;
+
+    @media (min-width: 1024px) {
+        width: 500px;
+        margin-top: 300px;
+    }
+`;
