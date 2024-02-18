0266bb0964a0ef8de1965cee1db929df7db16a7b
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2024-02-18 15:01:45 +0900

    ✨ : 취미 추천 페이지 퍼블리싱

---
+++ src/components/recommend/ResultBox.tsx
@@ -0,0 +1,189 @@
+import styled from 'styled-components';
+
+import { hobbies } from 'components/_common/hobbies';
+
+const ResultBox = ({
+    ranking,
+    hobby,
+    similarity,
+}: {
+    ranking: number;
+    hobby: string;
+    similarity: number;
+}) => {
+    return (
+        <Wrapper className={`rank${ranking}`}>
+            <Ranking className={`rank${ranking}`}>
+                <p>{ranking}위</p>
+            </Ranking>
+            <Text>
+                <h1>{hobby}</h1>
+                <p>유사도 {similarity}%</p>
+                <AddButton type="button">서랍장에 추가하기</AddButton>
+            </Text>
+            <IconCircle className={`rank${ranking}`}>
+                <Icon>
+                    <img src={hobbies[hobby]} alt={hobby} />
+                </Icon>
+            </IconCircle>
+        </Wrapper>
+    );
+};
+
+export default ResultBox;
+
+const Wrapper = styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 15px 16px 15px 50px;
+    margin-bottom: 30px;
+
+    border-radius: 8px;
+    box-shadow: var(--popup-shadow);
+    background-color: var(--blue1);
+    position: relative;
+
+    &.rank1 {
+        background-color: var(--blue1);
+    }
+    &.rank2 {
+        background-color: var(--blue2);
+    }
+    &.rank3 {
+        background-color: var(--blue3);
+    }
+
+    @media (min-width: 1024px) {
+        width: 460px;
+        padding: 20px 25px 20px 90px;
+        margin-bottom: 40px;
+    }
+`;
+
+const Ranking = styled.div`
+    width: 64px;
+    height: 52px;
+    clip-path: polygon(0 0, 100% 0, 47% 100%, 0% 100%);
+    box-shadow: var(--popup-shadow);
+
+    position: absolute;
+    top: -8px;
+    left: -10px;
+
+    p {
+        font-family: nanum-bold;
+        font-size: 20px;
+        margin: 10px 5px;
+    }
+
+    &.rank1 {
+        background-color: var(--pink);
+    }
+    &.rank2 {
+        background-color: var(--purple1);
+    }
+    &.rank3 {
+        background-color: var(--purple2);
+    }
+
+    @media (min-width: 1024px) {
+        width: 80px;
+        height: 70px;
+
+        p {
+            font-family: nanum-bold;
+            font-size: 24px;
+            margin: 18px 7px;
+        }
+    }
+`;
+
+const Text = styled.div`
+    margin-right: 50px;
+
+    h1 {
+        font-size: 24px;
+    }
+    p {
+        margin: 15px 0;
+        font-size: 18px;
+    }
+
+    @media (min-width: 1024px) {
+        h1 {
+            font-size: 26px;
+        }
+        p {
+            margin: 15px 0;
+            font-size: 20px;
+        }
+    }
+`;
+
+const AddButton = styled.button`
+    padding: 5px 10px;
+    font-size: 12px;
+    color: white;
+    white-space: nowrap;
+    border-radius: 4px;
+    background-color: var(--blue4);
+
+    @media (min-width: 1024px) {
+        font-size: 15px;
+
+        &:hover {
+            cursor: pointer;
+            opacity: 0.7;
+            transition: opacity 200ms ease-in-out;
+        }
+    }
+`;
+
+const IconCircle = styled.div`
+    width: 100px;
+    height: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    border-radius: 50%;
+    background-color: var(--pink);
+    border: 5px solid var(--orange);
+    box-shadow: var(--dropdown-shadow);
+
+    &.rank1 {
+        background-color: var(--pink);
+    }
+    &.rank2 {
+        background-color: var(--purple1);
+    }
+    &.rank3 {
+        background-color: var(--purple2);
+    }
+
+    @media (min-width: 1024px) {
+        width: 120px;
+        height: 120px;
+        transition: transform 200ms ease-in-out;
+
+        &:hover {
+            cursor: pointer;
+            transform: rotate(90deg);
+        }
+    }
+`;
+
+const Icon = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-grow: 0.6;
+
+    img {
+        display: block;
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+`;
--- src/pages/RecommendPage.tsx
+++ src/pages/RecommendPage.tsx
@@ -1,12 +1,40 @@
 import styled from 'styled-components';
+
+import { Container } from 'components/_common/pageLayout';
+
 import NavBar from 'components/_common/Navbar';
+import Header from 'components/_common/Header';
+import ResultBox from 'components/recommend/ResultBox';
 
 const RecommendPage = () => {
     return (
-        <div>
-            <NavBar></NavBar>
-        </div>
+        <Wrapper>
+            <NavBar />
+            <Container>
+                <Header bold="민지" reg="님을 위한 취미는 . . . " />
+                <Exp>아이콘을 클릭하여 바로 취미 활동을 시작해보세요!</Exp>
+                <ResultBox ranking={1} hobby="등산" similarity={88} />
+                <ResultBox ranking={2} hobby="베이킹" similarity={70} />
+                <ResultBox ranking={3} hobby="공예" similarity={68} />
+            </Container>
+        </Wrapper>
     );
 };
 
 export default RecommendPage;
+
+const Wrapper = styled.div`
+    position: relative;
+`;
+
+const Exp = styled.p`
+    font-family: nanum-light;
+    font-size: 14px;
+    margin: 8px 0 20px 40px;
+    white-space: nowrap;
+
+    @media (min-width: 1024px) {
+        font-size: 16px;
+        margin: 8px 0 40px 90px;
+    }
+`;
