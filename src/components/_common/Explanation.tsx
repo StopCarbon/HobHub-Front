408c28c88190498719f9f6133ec3b1958b4d65ab
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2024-02-26 12:55:49 +0900

    💄 : Explanation 공용 컴포넌트 생성

---
+++ src/components/_common/Explanation.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+const Explanation = ({ text }: { text: string }) => {
+    return <Exp>{text}</Exp>;
+};
+
+export default Explanation;
+
+const Exp = styled.p`
+    font-family: nanum-light;
+    font-size: 14px;
+    margin: 8px 0 25px 45px;
+    white-space: nowrap;
+
+    @media (min-width: 1024px) {
+        font-size: 16px;
+        margin: 8px 0 40px 90px;
+    }
+`;
--- src/pages/RecommendPage.tsx
+++ src/pages/RecommendPage.tsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
 import { Container } from 'components/_common/pageLayout';
-
 import NavBar from 'components/_common/Navbar';
 import Header from 'components/_common/Header';
+import Explanation from 'components/_common/Explanation';
 import ResultBox from 'components/recommendpage/ResultBox';
 
 const RecommendPage = () => {
@@ -12,7 +12,7 @@ const RecommendPage = () => {
             <NavBar />
             <Container>
                 <Header bold="민지" reg="님을 위한 취미는 . . . " />
-                <Exp>아이콘을 클릭하여 바로 취미 활동을 시작해보세요!</Exp>
+                <Explanation text="아이콘을 클릭하여 바로 취미 활동을 시작해보세요!" />
                 <ResultWrapper>
                     <ResultBox ranking={1} hobby="등산" similarity={88} />
                     <ResultBox ranking={2} hobby="베이킹" similarity={70} />
@@ -27,18 +27,6 @@ export default RecommendPage;
 
 const Wrapper = styled.div`
     position: relative; // PC 버전 container 때문
-`;
-
-const Exp = styled.p`
-    font-family: nanum-light;
-    font-size: 14px;
-    margin: 8px 0 25px 45px;
-    white-space: nowrap;
-
-    @media (min-width: 1024px) {
-        font-size: 16px;
-        margin: 8px 0 40px 90px;
-    }
 `;
 
 const ResultWrapper = styled.div`
