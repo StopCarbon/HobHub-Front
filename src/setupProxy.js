efbbd9404ebcfa2ccbd7ead3f4caf85dda57958c
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2024-04-16 12:30:06 +0900

    🐛 : api 연결 중

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,38 +2,36 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     // /user로 시작하는 경로 (유저 정보 저장, 유저 취미 저장 & 추가, 유저 취미 리스트 반환)
-    app.use(
-        createProxyMiddleware('/user', {
-            target: process.env.REACT_APP_API_URL,
-            changeOrigin: true,
-        }),
-    );
-
+    // app.use(
+    //     createProxyMiddleware('/user', {
+    //         target: process.env.REACT_APP_API_URL,
+    //         changeOrigin: true,
+    //     }),
+    // );
     // /board로 시작하는 경로
-    app.use(
-        createProxyMiddleware('/board', {
-            target: process.env.REACT_APP_API_URL,
-            changeOrigin: true,
-        }),
-    );
-
+    // app.use(
+    //     createProxyMiddleware('/board', {
+    //         target: process.env.REACT_APP_API_URL,
+    //         changeOrigin: true,
+    //     }),
+    // );
     // for FLASK
-    app.use(
-        createProxyMiddleware('/send_message', {
-            target: process.env.REACT_APP_FLASK_URL,
-            changeOrigin: true,
-        }),
-    );
-    app.use(
-        createProxyMiddleware('/webhook', {
-            target: process.env.REACT_APP_FLASK_URL,
-            changeOrigin: true,
-        }),
-    );
-    app.use(
-        createProxyMiddleware('/hobbylist', {
-            target: process.env.REACT_APP_FLASK_URL,
-            changeOrigin: true,
-        }),
-    );
+    // app.use(
+    //     createProxyMiddleware('/send_message', {
+    //         target: process.env.FLASK_URL,
+    //         changeOrigin: true,
+    //     }),
+    // );
+    // app.use(
+    //     createProxyMiddleware('/webhook', {
+    //         target: process.env.FLASK_URL,
+    //         changeOrigin: true,
+    //     }),
+    // );
+    // app.use(
+    //     createProxyMiddleware('/hobbylist', {
+    //         target: process.env.FLASK_URL,
+    //         changeOrigin: true,
+    //     }),
+    // );
 };
