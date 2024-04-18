722e63497620827d79448f2a5c4a839557c2a28e
Yebin Choi <109021332+beenvyn@users.noreply.github.com>
2024-04-18 15:25:18 +0900

    🐛

--- src/pages/ChatbotPage.tsx
+++ src/pages/ChatbotPage.tsx
@@ -17,7 +17,7 @@ import { UserInfoAtom } from 'recoil/User';
 
 import { http } from 'flask_api/http';
 
-document.cookie = 'session=bar; SameSite=None; Secure';
+document.cookie = 'SameSite=None; Secure';
 
 const history = Array.from({ length: 30 }, (_, index) => ({
     order: index,
